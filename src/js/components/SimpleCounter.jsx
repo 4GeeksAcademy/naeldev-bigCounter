b5ce6519b35dd3f7f8e3431aa9922fc2b870057f
Nael <naeldiaz@proton.me>
2025-04-22 16:42:34 +0000

    Falta el último punto

--- src/js/components/SimpleCounter.jsx
+++ src/js/components/SimpleCounter.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { CiClock2 } from "react-icons/ci";
 
-function SimpleCounter({ numberSix,numberFive,numberFour, numberThree, numberTwo, numberOne }) {
+function SimpleCounter({ numberSix,numberFive,numberFour, numberThree, numberTwo,
+     numberOne,pauseCounter,continueCounter,resetCounter,reduceCounter,insertTime}) {
     return (
-        <div className="counter">
-            <div className="clock">
+        <>
+        <div className="counter ">
+            <div className=" clock">
             <CiClock2 />
             </div>
             <div className='six'>{numberSix % 10}</div>
@@ -13,12 +15,19 @@ function SimpleCounter({ numberSix,numberFive,numberFour, numberThree, numberTwo
             <div className="three">{numberThree % 10}</div>
             <div className="two">{numberTwo% 10}</div>
             <div className="one">{numberOne % 10}</div>
-            <div className="btn-group  d-flex justify-content-center" role="group" aria-label="Basic mixed styles example">
-                <button type="button" className="btn btn-danger">Left</button>
-                <button type="button" className="btn btn-warning">Middle</button>
-                <button type="button" className="btn btn-success">Right</button>
+            <div className='d-flex justify-content-center'>
+                <button onClick={pauseCounter} type="button" className="btn btn-danger mx-2">Pausar</button>
+                <button onClick={continueCounter} type="button" className="btn btn-warning mx-2">Reanudar</button>
+                <button onClick= {resetCounter} type="button" className="btn btn-success mx-2">Reiniciar</button>
+                <button onClick={reduceCounter}  type="button" className="btn btn-primary mx-2">Decremento</button>
+                
             </div>
+            
         </div>
+            
+              
+          
+    </>
     );
 }
 
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -11,31 +11,57 @@ import '../styles/index.css'
 // components
 import SimpleCounter from './components/SimpleCounter';
 
-//let counter = 0;
+let counter = 0;
+let control;
 let number = 985675;
 
 
 
-setInterval(() => {
-    const squareSix = Math.floor(number / 100000)
-    const squareFive = Math.floor(number / 10000);
-    const squareFour = Math.floor(number / 1000);
-    const squareThree = Math.floor(number / 100);
-    const squareTwo = Math.floor(number / 10);
-    const squareOne = Math.floor(number / 1);
 
-    function resetCounter (number){
-      
-    }
+
+  function timeControl (){
+      control =setInterval(() => {
+ 
+    const squareSix = Math.floor(counter / 100000)
+    const squareFive = Math.floor( counter/ 10000);
+    const squareFour = Math.floor(counter / 1000);
+    const squareThree = Math.floor(counter / 100);
+    const squareTwo = Math.floor(counter / 10);
+    const squareOne = Math.floor(counter / 1);
+
+    
     //counter++;
-    number--;
+    counter++;
     
     ReactDOM.createRoot(document.getElementById('root')).render(
       <React.StrictMode>
-       <SimpleCounter numberOne={squareOne} numberTwo={squareTwo} numberThree={squareThree} numberFour={squareFour} numberFive = {squareFive} numberSix = {squareSix} />
+        <SimpleCounter
+          numberOne={squareOne}
+          numberTwo={squareTwo}
+          numberThree={squareThree}
+          numberFour={squareFour}
+          numberFive={squareFive}
+          numberSix={squareSix} 
+          pauseCounter ={()=>{
+            clearInterval(control);  
+          }}
+          continueCounter={timeControl}
+          resetCounter={()=>{
+            counter=0;
+            clearInterval(control);
+            timeControl();
+          }}
+          reduceCounter={()=>{
+            counter=number;
+            counter--;
+          }}
+          />
       </React.StrictMode>,
-    )
-}, 1000);
+    );
+  }, 1000);
+}
+
+timeControl();
 
 
 
